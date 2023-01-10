4e92de84568f73d97fd07ac5ebfe24cef0a153ca
mm_tulay <109350207+Maksym-Tulay@users.noreply.github.com>
2023-01-10 09:33:42 +0200

    task--01__done

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,62 @@
-console.log("asaf")
+// refs
+
+const refs = {
+    startButton: document.querySelector('button[data-start]'),
+    stopButton: document.querySelector('button[data-stop]')
+};
+
+// fn for getting color
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+// fn for update bg color
+
+function updateBodyBgColor(color) {
+    document.body.style.backgroundColor = color;
+}
+
+// class with properties for start/stop changing body bg color + disabling start btn while started and stop btn when stopped
+
+class ColorChanger {
+    constructor (updateBodyBgColor) {
+        this.isActive = false;
+        this.interval = null;
+        this.updateBodyBgColor = updateBodyBgColor;
+        refs.stopButton.disabled = true;
+    }
+
+    startChangeBgColor() {
+        if (this.isActive) {
+           return; 
+        }
+        this.isActive = true;
+        this.interval = setInterval(() => updateBodyBgColor(getRandomHexColor()), 1000);
+
+        refs.startButton.disabled = true;
+        refs.stopButton.disabled = false;
+    }
+
+    stopChangeBgColor() {
+        this.isActive = false;
+        clearInterval(this.interval);
+
+        refs.startButton.disabled = false;
+        refs.stopButton.disabled = true;
+    }
+}
+
+// expression of class
+
+const bgColorChanger = new ColorChanger();
+
+// add eventListeners for buttons
+
+refs.startButton.addEventListener('click', () => bgColorChanger.startChangeBgColor());
+refs.stopButton.addEventListener('click', () => bgColorChanger.stopChangeBgColor());
+
+
+
+
+
